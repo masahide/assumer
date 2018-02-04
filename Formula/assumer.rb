dad875c23dfeadf1c7a43214296826e11b72ff86
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-04 16:32:17 +0900

    assumer version v0.1.4

--- Formula/assumer.rb
+++ Formula/assumer.rb
@@ -1,9 +1,9 @@
 class Assumer < Formula
   desc "AWS assume role credential wrapper"
   homepage "https://github.com/masahide/assumer"
-  url "https://github.com/masahide/assumer/releases/download/v0.1.3/assumer_Darwin_x86_64.tar.gz"
-  version "0.1.3"
-  sha256 "151c14052c78b2bb86a2986c51b4b17f328e2057506c23905153dd2fe311cd21"
+  url "https://github.com/masahide/assumer/releases/download/v0.1.4/assumer_Darwin_x86_64.tar.gz"
+  version "0.1.4"
+  sha256 "288b9be6fb24331b43b8f3b28cccbbffbcc6a58d39f10d32a20f2692bcb10358"
 
   def install
     bin.install "assumer"
